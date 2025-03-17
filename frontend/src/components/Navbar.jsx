1822f17e495047dd91af1f0494a17654b58ca555
AriesJ <ajtagle12@gmail.com>
2025-03-17 18:56:33 +0800

    adding mui

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="flex justify-center">
-      <div className="max-w-7xl grid grid-cols-15 ">
+      <div className="max-w-7xl grid grid-cols-17 gap-4">
         <div className="col-span-3">
           <img src="#" alt="Brand Icon" />
           <span>Brand Name</span>
         </div>
-        <form className="col-span-4">
+        <form className="col-span-4 col-start-5">
           <input type="text" placeholder="Search" />
           <i>Search Icon</i>
         </form>
-        <div className="col-span-2">
+        <div className="col-span-2 col-start-11">
           <a href="#">Heart</a>
           <a href="#">Cart</a>
         </div>
