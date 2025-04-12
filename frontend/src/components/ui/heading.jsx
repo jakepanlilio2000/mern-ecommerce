5c60add7280196d902fdb4a839ba48662ebbcbda
AriesJ <ajtagle12@gmail.com>
2025-04-12 10:17:39 +0800

    heading component

--- frontend/src/components/ui/heading.jsx
+++ frontend/src/components/ui/heading.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-function Heading({variant}) {
-    const classes = "";
-    const defaultClasses = "text dark:text";
-  return (
-    <span className={classes}>Heading</span>
-  )
+function Heading({ variant = 'small' }) {
+  let classes = "text dark:text "; // default classes
+
+  switch (variant) {
+    case 'special-announcement':
+      classes += "text-red-600 text-2xl font-bold uppercase";
+      break;
+    case 'special-announcement-small':
+      classes += "text-red-500 text-lg font-semibold uppercase";
+      break;
+    case 'big':
+      classes += "text-4xl font-bold";
+      break;
+    case 'small':
+    default:
+      classes += "text-sm";
+      break;
+  }
+
+  return <span className={classes}>Heading</span>;
 }
 
-export default Heading
+export default Heading;
