6895ec3b6c1bf6a4aba823bbe3f8fc25e27205c8
AriesJ <ajtagle12@gmail.com>
2025-04-14 10:35:45 +0800

    convert buycard

--- frontend/src/components/parts/buyCard.tsx
+++ frontend/src/components/parts/buyCard.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
 
+interface BuyCardProps{
+  id: number,
+  title: string,
+  origPrice: number,
+  stars: number,
+  discount: number //must be a float
+}
 
-function BuyCard({id, title, origPrice, stars, discount }) {
+
+function BuyCard({id, title, origPrice, stars, discount }: BuyCardProps) {
+  const generateStar = (number: number) => {
+    
+  }
   // must recieve id as number, title as string, origPrice as number, stars as number, discount as float
   // why we not using ts?
   const sellingPrice = origPrice * discount
