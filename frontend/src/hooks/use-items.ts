fbcc362a82f8ddc8ab78d5c6177f4a7b6d0c6630
AriesJ <ajtagle12@gmail.com>
2025-04-23 00:06:45 +0800

    krazy jsx

--- frontend/src/hooks/use-items.ts
+++ frontend/src/hooks/use-items.ts
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react"
 import { Items } from "../types/global"
+import axios from 'axios';
 
 function useTopItems() {
     const [topItems, setTopItems] = useState<Items[]>([]);
-    useEffect(()=> {
-        
+    useEffect( ()=> {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get<Items[]>("/top-items");
+                setTopItems(res.data);
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        fetchData();
     }, []);
     return topItems;
 }
