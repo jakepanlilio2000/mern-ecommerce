f9a684466ddac3143462d8e29e109aaabde419a5
AriesJ <ajtagle12@gmail.com>
2025-04-22 12:34:03 +0800

    fix carousell

--- frontend/src/components/parts/carousell.jsx
+++ frontend/src/components/parts/carousell.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from "react";
+import Slider from "react-slick";
 
 function Carousell(imagePaths) {
-    
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+  };
   return (
-    <div className="">
+    <div className="slider-container">
+      <Slider {...settings}>
         {imagePaths.map((path, index) => {
-            return <img src={path} key={index} alt="carousell picture" />
-        })} 
+          return (
+            <div>
+              <img src={path} key={index} alt="carousell picture" />
+            </div>
+          );
+        })}
+      </Slider>
     </div>
-  )
+  );
 }
 
-export default Carousell
+export default Carousell;
