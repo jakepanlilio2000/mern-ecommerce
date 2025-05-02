15a87ffdc45ba3cb9af424e6bf2f3e8b0278cf50
AriesJ <ajtagle12@gmail.com>
2025-05-02 19:52:13 +0800

    idk carousel its buggy

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,9 @@
 import Image from "next/image";
+import Carousel from "./components/carousel";
+
+
+const SLIDE_COUNT = 3
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 export default function Home() {
   return (
@@ -8,7 +13,7 @@ export default function Home() {
         <h1>20% off everything</h1>
         {/* Carousel */}
         <section className="flex justify-center items-center rounded-b-[250px] bg-linear-to-b from-primary to-secondary border-black border">
-          
+          <Carousel slides={SLIDES}></Carousel>
         </section>
         {/* best seller */}
         {/* new arrivals */}
