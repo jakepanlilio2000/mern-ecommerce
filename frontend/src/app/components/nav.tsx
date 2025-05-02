8bed859871807d11deaaca2fdf004e735337ee1f
AriesJ <ajtagle12@gmail.com>
2025-05-02 08:25:32 +0800

    adding navbar

--- frontend/src/app/components/nav.tsx
+++ frontend/src/app/components/nav.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import AppLogo from "./ui/app-logo";
 
-function Nav() {
+interface NavProps {
+  companyName: string;
+}
+
+function Nav({ companyName }: NavProps) {
   return (
     <nav>
-      <div></div>
+      <div>
+        <div>
+          <AppLogo></AppLogo>
+          <span>{companyName}</span>
+        </div>
+      </div>
       <div>
         <Link href="/">Home</Link>
-
         <Link href="/about">About Us</Link>
-
         <Link href="/contact">Contact Us</Link>
-
-        <Link href="/products">Products</Link>
-
+        <Link href="/product">Products</Link>
         <Link href="/vouchers">Vouchers</Link>
       </div>
     </nav>
---
+++ frontend/src/app/components/ui/app-logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AppLogo() {
+  return (
+    <img src="https://picsum.photos/id/237/200/300" alt="App Logo" className="rounded-full h-5 w-5"/>
+  )
+}
+
+export default AppLogo
