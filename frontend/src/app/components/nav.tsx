128cc2a0316b8065db3e5683ecbcb8f08c9a4758
AriesJ <ajtagle12@gmail.com>
2025-05-07 15:48:48 +0800

    done navbar

--- frontend/src/app/components/nav.tsx
+++ frontend/src/app/components/nav.tsx
@@ -44,7 +44,7 @@ function Nav({ companyName }: NavProps) {
             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
               <button
                 type="button"
-                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset"
+                className="relative inline-flex items-center justify-center rounded-md p-2 text-black hover:cursor-pointer hover:text-primary focus:outline-hidden focus:ring-inset"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
                 onClick={() => setNavOpen(!navOpen)}
@@ -83,17 +83,18 @@ function Nav({ companyName }: NavProps) {
                 </svg>
               </button>
             </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-1 sm:flex-0 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex shrink-0 items-center">
                 <AppLogo></AppLogo>
-                <span className="hidden sm:inline-block text-primary ml-2">{companyName}</span>
-              </div>
-              <div className="hidden sm:ml-6 sm:block">
-                <SearchBar></SearchBar>
+                <span className="hidden sm:inline-block text-primary ml-2">
+                  {companyName}
+                </span>
               </div>
             </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <SearchBar></SearchBar>
+            </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              
               <div className="relative ml-3">
                 <div>
                   <button
@@ -157,10 +158,10 @@ function Nav({ companyName }: NavProps) {
         </div>
       </div>
       <div className="bg-secondary">
-        <div className="hidden sm:block max-w-7xl py-2">
+        <div className="hidden sm:block max-w-7xl py-2 mx-auto">
           <div className="flex space-x-4 gap-4 justify-center items-center">
             {/* class of active:  rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white*/}
-            {/* class of inactive: rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white */}
+            {/* class of inactive: ro unded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white */}
             {NAV_ITEMS.map((item, index) => {
               return (
                 <Link
@@ -177,35 +178,23 @@ function Nav({ companyName }: NavProps) {
       </div>
 
       <div
-        className={`sm:hidden ${navOpen ? "block" : "hidden"}`}
+        className={`sm:hidden ${navOpen ? "block" : "hidden"} bg-secondary`}
         id="mobile-menu"
       >
         <div className="space-y-1 px-2 pt-2 pb-3">
-          <Link
-            href="#"
-            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-            aria-current="page"
-          >
-            Dashboard
-          </Link>
-          <Link
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Team
-          </Link>
-          <Link
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Projects
-          </Link>
-          <Link
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Calendar
-          </Link>
+          {/* active class: "block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" */}
+          {/* inactive: block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white */}
+          {NAV_ITEMS.map((item, index) => {
+            return (
+              <Link
+                href={item.href}
+                className="block rounded-md px-3 py-2 text-base font-medium text-black hover:underline"
+                aria-current="page"
+              >
+                {item.name}
+              </Link>
+            );
+          })}
         </div>
       </div>
     </nav>
